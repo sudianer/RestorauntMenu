b581ee0e307bb663725fbdeeb96525b9d7538219
Filatov Oleg <sudianer@gmail.com>
2020-03-19 11:24:40 +0400

    Began working with Entity framework

---
+++ Data/AppDbContent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestorauntMenu.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestorauntMenu.Data
+{
+    public class AppDbContent: DbContext
+    {
+        public AppDbContent(DbContextOptions<AppDbContent> options): base(options)
+        {
+        }
+
+        public DbSet<Dish> Dish { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,20 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RestorauntMenu.Data;
 using RestorauntMenu.Data.Interfaces;
 using RestorauntMenu.Data.Mocks;
+using Microsoft.EntityFrameworkCore;
 
 namespace RestorauntMenu
 {
     public class Startup
     {
+        private IConfigurationRoot _confString;
+
+        public Startup(IWebHostEnvironment hostEnv)
+        {
+            _confString = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
+            services.AddDbContext<AppDbContent>(options => options.UseSqlServer(_confString.GetConnectionString("DefaultConnection")));
             services.AddTransient<IDishes, MockDishes>();
             services.AddMvc(option => option.EnableEndpointRouting = false);
 
