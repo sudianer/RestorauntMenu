ea325491a319c8cb36a65069af510df0c9cc08e6
Filatov Oleg <sudianer@gmail.com>
2020-03-31 13:34:51 +0400

    Создал формы редактирования, просмотра, создания нового блюда. Пока что в процессе

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using RestorauntMenu.Data.Interfaces;
 using RestorauntMenu.ViewModels;
 using RestorauntMenu.Data;
+using Microsoft.EntityFrameworkCore;
+using RestorauntMenu.Data.Models;
 
 namespace RestorauntMenu.Controllers
 {
@@ -13,20 +15,80 @@ namespace RestorauntMenu.Controllers
     /// Контроллер главной страницы
     /// </summary>
     public class HomeController: Controller
-    {
-        private IDishes _dishes;
+    {      
         AppDbContext _db;
         public HomeController(AppDbContext db)
         {
             _db = db;        
         }
 
-        public ViewResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View(_db.Dish.ToList());
+            return View(await _db.Dish.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Dish Dish)
+        {
+            _db.Dish.Add(Dish);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        /// <summary>
+        /// Показывает форму с подробной информацией о блюде
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id != null)
+            {
+                Dish dish = await _db.Dish.FirstOrDefaultAsync(p=> p.Id == id);
+                if(dish != null)
+                    return View(dish);
+                
+            }
+            return NotFound();
+        }
+        
+        /// <summary>
+        /// Показывает форму редактирования объекта
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Dish dish = await _db.Dish.FirstOrDefaultAsync(p => p.Id == id);
+                if (dish != null)
+                    return View(dish);
+            }
+            return NotFound();
         }
 
+        /// <summary>
+        /// обновляет объект в базе данных
+        /// </summary>
+        /// <param name="dish"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Edit(Dish dish)
+        {
+            _db.Dish.Update(dish);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+    
+    
+    
     }
+
+
 
 
 }
--- Data/Models/Dish.cs
+++ Data/Models/Dish.cs
@@ -18,32 +18,32 @@ namespace RestorauntMenu.Data.Models
         [ScaffoldColumn(false)]
         public string CreationDate { get; }
         [Display(Name = "Название")]
-        [StringLength(4)]
+        [StringLength(40)]
         [Required(ErrorMessage = "Длина названия не менее 4 символов")]
         public string Title { get; set; }
         [Display(Name = "Состав")]
-        [StringLength(1)]
+        [StringLength(120)]
         [Required(ErrorMessage = "Длина состава не менее 1 символа")]
         public string Ingredients { get; set; }
         [Display(Name = "Описание")]
-        [StringLength(1)]
+        [StringLength(100)]
         [Required(ErrorMessage = "Длина описания не менее 1 символа")]
         public string Description { get; set; }
         [Display(Name = "Цена")]
-        [StringLength(1)]
+        [StringLength(100)]
         [Required(ErrorMessage = "Длина цены не менее 1 символа")]
         public double Price { get; set; }
         [Display(Name = "Вес")]
-        [StringLength(1)]
+        [StringLength(10)]
         [Required(ErrorMessage = "Длина веса не менее 1 символа")]
         public int Weight { get; set; }
         [Display(Name = "Калории")]
-        [StringLength(1)]
+        [StringLength(10)]
         [Required(ErrorMessage = "Длина калорий не менее 1 символа")]
         public int Calories { get; set; }
-        [Display(Name = "Время создания")]
-        [StringLength(1)]
-        [Required(ErrorMessage = "Длина времени создания не менее 1 символа")]
+        [Display(Name = "Время приготовления")]
+        [StringLength(10)]
+        [Required(ErrorMessage = "Длина времени приготовления не менее 1 символа")]
         public int TimeToMake { get; set; }
     }
 }
